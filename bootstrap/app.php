5d1940874c9646b3ac3297233a2a5a906190b620
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2026-07-12 17:02:11 +0545

    Changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,10 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->trustProxies(
             '*',
-            Request::HEADER_x_FORWARDED_FOR |
-            Request::HEADER_x_FORWARDED_HOST |
-            Request::HEADER_x_FORWARDED_PORT |
-            Request::HEADER_x_FORWARDED_PROTO
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
